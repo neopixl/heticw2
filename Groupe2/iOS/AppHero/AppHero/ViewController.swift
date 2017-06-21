d4ba94a1b65bee6a65ba967ffc0b5eaed3a1c20d
Yvan Moté <yvan@neopixl.com>
2017-06-21 13:54:54 +0200

    First step for iOS Project

---
+++ Groupe2/iOS/AppHero/AppHero/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  AppHero
+//
+//  Created by Yvan Moté on 21/06/2017.
+//  Copyright © 2017 Neopixl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	@IBOutlet weak var loginTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	// MARK: - Private methods
+	
+	func showAlert(_ message: String) {
+		let alertController = UIAlertController(title: "information", message: message, preferredStyle: .alert)
+		
+		alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler:nil))
+		
+		self.present(alertController, animated: true, completion: nil)
+		
+		
+	}
+	
+	// MARK: - Action methods
+	
+	// sender = bouton qui a intercepté le tap
+	@IBAction func signin(_ sender: Any) {
+		
+		if let login = loginTextField.text, let password = passwordTextField.text {
+			
+			let credentialsAreNotEmpty = !login.isEmpty && !password.isEmpty
+			
+			if credentialsAreNotEmpty {
+				showAlert("Vous êtes connecté !")
+			} else {
+				
+			}
+			
+		}
+		
+	}
+	
+	@IBAction func signup(_ sender: Any) {
+		
+	}
+	
+	
+	
+	
+	
+
+
+}
+
